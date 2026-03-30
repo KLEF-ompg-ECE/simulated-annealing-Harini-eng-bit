519f4aa595ce8f59f3d6b9217ae2dd05633aafc5
2310040065_Divya <2310040065@klh.edu.in>
2026-03-30 09:57:11 +0530

    commit

--- sa_timetable.py
+++ sa_timetable.py
@@ -235,3 +235,31 @@ if __name__ == "__main__":
     # print_timetable(tt2)
     # print(f"  Final clashes : {clashes2}")
     # save_plot(cl2, tl2, "plots/experiment_2a.png", "cooling_rate=0.80")   # <- change filename
+# Cooling rate 0.80
+tt2, clashes2, cl2, tl2 = run_sa(
+    initial_temp=100.0, cooling_rate=0.80,
+    min_temp=0.1, max_iterations=5000, seed=42
+)
+print_timetable(tt2)
+print(f"Final clashes : {clashes2}")
+save_plot(cl2, tl2, "plots/experiment_2a.png", "cooling_rate=0.80")
+
+
+# Cooling rate 0.95
+tt3, clashes3, cl3, tl3 = run_sa(
+    initial_temp=100.0, cooling_rate=0.95,
+    min_temp=0.1, max_iterations=5000, seed=42
+)
+print_timetable(tt3)
+print(f"Final clashes : {clashes3}")
+save_plot(cl3, tl3, "plots/experiment_2b.png", "cooling_rate=0.95")
+
+
+# Cooling rate 0.995
+tt4, clashes4, cl4, tl4 = run_sa(
+    initial_temp=100.0, cooling_rate=0.995,
+    min_temp=0.1, max_iterations=5000, seed=42
+)
+print_timetable(tt4)
+print(f"Final clashes : {clashes4}")
+save_plot(cl4, tl4, "plots/experiment_2c.png", "cooling_rate=0.995")
